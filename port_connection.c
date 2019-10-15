aa70b483f02e7ebf78570111c06041395997d275
zhenwei pi <pizhenwei@bytedance.com>
2019-10-15 14:27:53 +0800

    fix compat with new kernel
    since linux-4.13, nf_register_hook has been removed by
    cf56c2f892a8a1870a8358114ad896772da7543a.
    linux-3.10 does NOT support nf_register_net_hook, so use kernel
    version macro to isolate these cases.
    
    Signed-off-by: zhenwei pi <pizhenwei@bytedance.com>

--- port_connection.c
+++ port_connection.c
@@ -1,7 +1,8 @@
 /*
-   build it as kernel module, and use it to find which process is visting destination port
+   build it as kernel module, and use it to find which process is
+   visting destination port
 
-   Copyright@Zhenwei Pi
+   Copyright @ zhenwei pi
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -26,6 +27,7 @@
 #include <linux/udp.h>
 #include <linux/inet.h>
 #include <linux/sysctl.h>
+#include <linux/version.h>
 
 static unsigned int min_port = 0;
 static unsigned int max_port = 65535;
@@ -47,23 +49,31 @@ static struct ctl_table port_conn_table[] = {
 	}
 };
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+unsigned int __port_connection_hookfn(void *priv,
+		struct sk_buff *skb,
+		const struct nf_hook_state *state)
+#else
 unsigned int __port_connection_hookfn(unsigned int hooknum,
 		struct sk_buff *skb,
 		const struct net_device *in,
 		const struct net_device *out,
 		int (*okfn)(struct sk_buff *))
+#endif
 {
 	struct iphdr *iph;
 	struct udphdr *udph;
 	iph = ip_hdr(skb);
 
-	//printk(KERN_INFO"%d, protocol = %d,  src IP %pI4\n", __LINE__, iph->protocol, &iph->saddr);
-	if (iph->protocol == IPPROTO_UDP)
-	{
+	/*
+	printk(KERN_INFO"%d, protocol = %d,  src IP %pI4\n",
+		__LINE__, iph->protocol, &iph->saddr);
+	*/
+	if (iph->protocol == IPPROTO_UDP) {
 		udph = udp_hdr(skb);
-		if (udph->dest == ntohs(sysctl_udp_port_connection))
-		{
-			printk(KERN_INFO"UDP : pid = %d, comm = %s\n\n", current->pid, current->comm);
+		if (udph->dest == ntohs(sysctl_udp_port_connection)) {
+			printk(KERN_INFO"UDP : pid = %d, comm = %s\n",
+				current->pid, current->comm);
 		}
 	}
 
@@ -82,15 +92,19 @@ static int __init nf_init(void)
 {
 	int err = 0;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+	err = nf_register_net_hook(&init_net, &port_conn_hook);
+#else
 	err = nf_register_hook(&port_conn_hook);
+#endif
 	if (err) {
 		printk(KERN_ERR"nf_register_hook() failed\n");
 		goto out;
 	}
 
-	ctl_header = register_net_sysctl(&init_net, "net/ipv4", port_conn_table);
-	if (ctl_header == NULL)
-	{
+	ctl_header = register_net_sysctl(&init_net, "net/ipv4",
+			port_conn_table);
+	if (ctl_header == NULL) {
 		err = -ENOMEM;
 		goto unregister_hook;
 	}
@@ -98,7 +112,11 @@ static int __init nf_init(void)
 	goto out;
 
 unregister_hook :
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+	nf_unregister_net_hook(&init_net, &port_conn_hook);
+#else
 	nf_unregister_hook(&port_conn_hook);
+#endif
 
 out :
 	return err;
@@ -106,12 +124,16 @@ out :
 
 static void __exit nf_exit(void)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
+	nf_unregister_net_hook(&init_net, &port_conn_hook);
+#else
 	nf_unregister_hook(&port_conn_hook);
+#endif
 	unregister_net_sysctl_table(ctl_header);
 	ctl_header = NULL;
 }
 
 module_init(nf_init);
 module_exit(nf_exit);
-MODULE_AUTHOR("PiZhenwei p_ace@126.com");
+MODULE_AUTHOR("zhenwei pi p_ace@126.com");
 MODULE_LICENSE("GPL");
